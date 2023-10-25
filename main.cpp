30c34e968517180556548cb42f6a2e161536eed8
MaksimMyson <maksimmyson33@gmail.com>
2023-10-25 15:13:47 +0300

    ex2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,29 @@ using namespace std;
 
 int main()
 {
-	
-	return 0;
+	int n;
+	cout << "Enter a number: ";
+	cin >> n;
+    int array[100][100]{};
+    int value = 0;
+    cout << "The array is: " << endl;
+    cin >> value;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            array[i][j] = value;
+            value = value + 1;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << array[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
+	
+	
